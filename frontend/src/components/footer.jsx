22e5a93ba99d7f7ba161e8f06c832366f7fb2020
Julie <julie.albini@edu.univ-fcomte.fr>
2025-01-21 17:49:21 +0100

    Footer creation

---
+++ frontend/src/components/footer.jsx
@@ -0,0 +1,21 @@
+import Flower from "../assets/icons/flower.svg";
+import Bouton from "./button"
+
+const Footer = () => {
+    return(
+        <div className="bg-bleuElectrique w-full py-10 p-32 flex gap-28 items-center justify-evenly" >
+            <img src={Flower} />
+            <div className="flex gap-12">
+                <p className="text-white text-base font-Roboto font-bold cursor-pointer">Terms of use</p>
+                <p className="text-white text-base font-Roboto font-bold cursor-pointer">Personal data protection</p>
+                <p className="text-white text-base font-Roboto font-bold cursor-pointer">Site map</p>
+            </div>
+            <div className="flex gap-6">
+                <Bouton variant="primary" text="Inscription" link="/login"/>
+                <Bouton variant="secondary" text="Connexion" link="/connexion"/>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
