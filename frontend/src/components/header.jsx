16b8317f40640e40b343e072c0d55a4ddc0306d4
Julie <julie.albini@edu.univ-fcomte.fr>
2025-01-22 19:01:02 +0100

    Add z-index in header

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -3,7 +3,7 @@ import Flower from "../assets/icons/flower.svg";
 
 const Header = () => {
     return(
-        <div className="bg-bleuElectrique w-full py-10 p-32 flex justify-between" >
+        <div className="bg-bleuElectrique w-full py-10 p-32 flex justify-between z-10 relative" >
             <div className="flex gap-16 items-center">
                 <img src={Flower} />
                 <p className="border w-fit px-4 py-1 rounded-full border-white text-white uppercase font-Gelasio text-base">Innovation</p>
