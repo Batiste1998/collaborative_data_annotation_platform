a8be998e521832d59bf34a0de8bb74b9ed32a88f
Julie <julie.albini@edu.univ-fcomte.fr>
2025-01-22 19:01:51 +0100

    RegistrerPage creation

---
+++ frontend/src/pages/auth/RegistrerPage.jsx
@@ -0,0 +1,19 @@
+import Header from '../../components/header'
+import Footer from '../../components/footer'
+import Registration from '../../components/registration'
+import Decoration from '../../components/decoration'
+
+const RegistrerPage = () => {
+    return(
+        <div className='bg-bleuElectrique py-auto h-screen'>
+            <Header/>
+            <div className="w-3/4 my-32 flex justify-evenly">
+                <Registration format="inscription"/>
+            </div>
+            <Decoration/>
+            <Footer/>
+        </div>
+    )
+}
+
+export default RegistrerPage
