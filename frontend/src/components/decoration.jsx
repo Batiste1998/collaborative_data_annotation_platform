9410d5e95b5138a2c279803126aa3c0e530a5387
Julie <julie.albini@edu.univ-fcomte.fr>
2025-01-22 19:00:29 +0100

    Decoration component creation

---
+++ frontend/src/components/decoration.jsx
@@ -0,0 +1,11 @@
+const Decoration = ({format}) => {
+    return(
+        <div>
+            <div className="absolute bottom-0 w-screen bg-violetBordure/80 h-52"></div>
+            <div className="absolute right-0 top-0 w-1/4 bg-white/50 h-screen z-0"></div>
+            <div className="absolute right-[15%] top-0 w-1/6 bg-roseBordure/50 h-screen z-0"></div>
+        </div>
+    )
+}
+
+export default Decoration
