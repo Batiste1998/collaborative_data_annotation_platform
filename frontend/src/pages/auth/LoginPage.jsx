814fa65e9367337fa68054ed680a8eb847d29e85
Julie <julie.albini@edu.univ-fcomte.fr>
2025-01-22 19:01:40 +0100

    Add Registration component in LoginPage

--- frontend/src/pages/auth/LoginPage.jsx
+++ frontend/src/pages/auth/LoginPage.jsx
@@ -1,10 +1,16 @@
 import Header from '../../components/header'
 import Footer from '../../components/footer'
+import Registration from '../../components/registration'
+import Decoration from '../../components/decoration'
 
 const LoginPage = () => {
     return(
-        <div>
+        <div className='bg-bleuElectrique py-auto h-screen'>
             <Header/>
+            <div className="w-3/4 my-32 flex justify-evenly">
+                <Registration format="connexion"/>
+            </div>
+            <Decoration/>
             <Footer/>
         </div>
     )
