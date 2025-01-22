297625d374a997033223145b267b5488d7cc05e6
Julie <julie.albini@edu.univ-fcomte.fr>
2025-01-22 19:00:12 +0100

    Button hover style

--- frontend/src/components/button.jsx
+++ frontend/src/components/button.jsx
@@ -2,18 +2,18 @@ const Bouton = ({ variant, text, link }) => {
     const getBackgroundClass = (variant) => {
         switch (variant) {
             case 'primary':
-                return 'bg-roseFond border-roseBordure';
+                return 'bg-roseFond border-roseBordure hover:bg-roseBordure hover:border-roseFond';
             case 'secondary':
-                return 'bg-violetFond border-violetBordure';
+                return 'bg-violetFond border-violetBordure hover:bg-violetBordure hover:border-violetFond';
             default:
-                return 'bg-roseFond border-roseBordure';
+                return 'bg-roseFond border-roseBordure hover:bg-roseBordure hover:border-roseFond';
         }
     };
 
     return (
         <div>
             <a href={link}>
-                <button className={`${getBackgroundClass(variant)} border py-2 px-8 rounded-full text-white font-bold font-Roboto`}>
+                <button className={`${getBackgroundClass(variant)} border py-2 px-8 rounded-full text-white font-bold font-Roboto transition duration-200 linear`}>
                     {text}
                 </button>
             </a>
