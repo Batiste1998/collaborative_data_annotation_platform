b42d0eea2bb53254974881bcd37df4ad537f605c
Julie <julie.albini@edu.univ-fcomte.fr>
2025-01-24 13:07:40 +0100

    CreateProjectPage finalization

--- frontend/src/pages/CreateProjectPage.jsx
+++ frontend/src/pages/CreateProjectPage.jsx
@@ -3,6 +3,8 @@ import Footer from '../components/footer'
 import Registration from '../components/registration'
 import Decoration from '../components/decoration'
 import Input from '../components/input'
+import Select from '../components/select'
+import Button from '../components/button'
 
 
 const CreateProjectPage = () => {
@@ -15,16 +17,23 @@ const CreateProjectPage = () => {
                 </div>
                 <Decoration className="absolute right-0"/>
             </div>
-            <div className="bg-white h-screen gap-6 py-12 flex justify-evenly">
+            <div className="bg-white h-fit gap-6 pt-12 pb-[20rem] flex justify-evenly">
                 <div className="flex flex-col gap-8">
                     <Input text="Project's name"/>
                     <Input text="Project's description"/>
-                    <Input text="Project's members"/>
+                    <Select label="" placeholder="Project's members" variant={"primary"} list={["Isabelle", "Nicolas", "Clément", "Virginie", "Batiste", "Julie"]}/>
+                    <div className='flex justify-end gap-3'>
+                        <Button variant={"primary"} text={"Create project"} link={""}/>
+                        <Button variant={"secondary"} text={"Import a file"} link={""}/>
+                    </div>
                 </div>
-                <div className="flex flex-col gap-8">
-                    <Input text="Project's name"/>
-                    <Input text="Project's description"/>
-                    <Input text="Project's members"/>
+                <div className='flex flex-col gap-8 before:content-[""] before:absolute before:bg-violetBordure before:opacity-40 before:w-screen before:h-[125%] before:z-0 z-10 relative before:rounded-bl-3xl before:translate-y-[-3rem]'>
+                    <Select label={"Task 1"} placeholder={"Project’s members"} variant={"secondary"} list={["Isabelle", "Nicolas", "Clément", "Virginie", "Batiste", "Julie"]}/>
+                    <Select label={"Task 2"} placeholder={"Project’s members"} variant={"secondary"} list={["Isabelle", "Nicolas", "Clément", "Virginie", "Batiste", "Julie"]}/>
+                    <Select label={"Task 3"} placeholder={"Project's members"} variant={"secondary"} list={["Isabelle", "Nicolas", "Clément", "Virginie", "Batiste", "Julie"]}/>
+                    <Select label={"Task 4"} placeholder={"Project's members"} variant={"secondary"} list={["Isabelle", "Nicolas", "Clément", "Virginie", "Batiste", "Julie"]}/>
+                    <Select label={"Task 5"} placeholder={"Project's members"} variant={"secondary"} list={["Isabelle", "Nicolas", "Clément", "Virginie", "Batiste", "Julie"]}/>
+                    <Select label={"Task 6"} placeholder={"Project's members"} variant={"secondary"} list={["Isabelle", "Nicolas", "Clément", "Virginie", "Batiste", "Julie"]}/>
                 </div>
             </div>
             <Footer/>
