0ff4ada55f941b7cedb82a5f64b52ba56566b43b
Julie <julie.albini@edu.univ-fcomte.fr>
2025-02-07 16:49:13 +0100

    Responsive RegistrationPage

--- frontend/src/components/registration.jsx
+++ frontend/src/components/registration.jsx
@@ -2,7 +2,7 @@ import Button from '../components/button'
 
 const Registration = ({ format }) => {
     return (
-        <div className={`bg-white/40 w-full sm:w-fit py-6 md:py-5 xl:py-10 pl-8 sm:px-14 md:px-10 xl:px-20 rounded-3xl flex flex-col ${format === 'inscription' ? 'gap-8' : 'gap-4 sm:gap-14'}`}>
+        <div className={`bg-white/40 w-full sm:w-fit py-6 md:py-5 xl:py-10 sm:pl-8 sm:px-14 md:px-10 xl:px-20 rounded-3xl flex flex-col items-center sm:items-stretch ${format === 'inscription' ? 'gap-8' : 'gap-4 sm:gap-14'}`}>
             {format === 'inscription' && (
                 <div className="flex gap-2 sm:gap-10 md:gap-20 justify-between flex-col sm:flex-row">
                     <label className="text-white md:text-xl xl:text-2xl font-Roboto">Username</label>
